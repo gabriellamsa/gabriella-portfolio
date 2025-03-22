54ded5df1f38c34057c7b6ec9e48f1368b74037e
gabriellamsa <gabii.marcelly@gmail.com>
2025-03-21 19:59:07 -0500

    feat: Add LinkedIn and GitHub links to Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Navebar } from "./components/Navbar";
 
 export default function App() {
   return (
-    <div className="overflow-x-hidden text-stone-600 antialised">
+    <div className="overflow-x-hidden antialised">
       <div className="fixed inset-0 z-10">
         <div className="absolute top-0 z-[-2] h-screen w-screen rotate-180 transform bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)]"></div>
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 import logo from "../assets/images/logo.png";
 
 export const Navebar = () => {
@@ -5,7 +6,26 @@ export const Navebar = () => {
     <nav className="flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
         <a href="/" aria-label="Home">
-          <img src={logo} className="mx-2" width={100} height={83} />
+          <img src={logo} className="mx-2" width={200} height={183} />
+        </a>
+      </div>
+
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a
+          href="https://www.linkedin.com/in/gabriellamsa/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="LinkedIn"
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href="https://github.com/gabriellamsa"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="GitHub"
+        >
+          <FaGithub />
         </a>
       </div>
     </nav>
