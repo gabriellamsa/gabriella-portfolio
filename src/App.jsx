73293a662bdd47f0f5d907cc19bffb182d1b5723
gabriellamsa <gabii.marcelly@gmail.com>
2025-03-26 15:09:01 -0500

    feat: apply global background

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,16 @@ import { Technologies } from "./components/Technologies";
 
 export default function App() {
   return (
-    <div className="overflow-x-hidden antialised">
-      <div className="inset-0 z-10">
-        <div className="absolute top-0 z-[-2] h-screen w-screen rotate-180 transform bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)]"></div>
+    <div className="overflow-x-hidden antialised relative">
+      <div id="global-background"></div>
 
-        <div className="container mx-auto px-8">
-          <Navebar />
-          <Hero />
-          <Technologies />
-          <Projects />
-          <Experience />
-          <Contact />
-        </div>
+      <div className="container mx-auto px-8 relative z-10">
+        <Navebar />
+        <Hero />
+        <Technologies />
+        <Projects />
+        <Experience />
+        <Contact />
       </div>
     </div>
   );
